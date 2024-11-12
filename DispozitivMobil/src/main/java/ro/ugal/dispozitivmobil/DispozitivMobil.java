d9fca43853b8579ab9453eb61d20920d8c9b6902
alexH <alexminzala09@gmail.com>
2024-11-12 18:23:57 +0200

    ala bala portocala

--- DispozitivMobil/src/main/java/ro/ugal/dispozitivmobil/DispozitivMobil.java
+++ DispozitivMobil/src/main/java/ro/ugal/dispozitivmobil/DispozitivMobil.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package ro.ugal.dispozitivmobil;
-
-/**
- *
- * @author Alex 2
- */
 public class DispozitivMobil {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("ala bala portocala");
     }
 }
