a34575b07a8ad8aed02d6d1ffc189609bc57ec0f
alexH <alexminzala09@gmail.com>
2024-11-12 17:42:20 +0200

    test

---
+++ DispozitivMobil/src/main/java/ro/ugal/dispozitivmobil/DispozitivMobil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ro.ugal.dispozitivmobil;
+
+/**
+ *
+ * @author Alex
+ */
+public class DispozitivMobil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
