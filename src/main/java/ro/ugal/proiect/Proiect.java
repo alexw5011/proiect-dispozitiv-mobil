4a10cbf2003d2136d5080e07cebb7c2a28b46cc2
im355 <im355@WIN-1.CTI.UGAL.RO>
2024-11-15 09:06:22 +0200

    Main

---
+++ src/main/java/ro/ugal/proiect/Proiect.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ro.ugal.proiect;
+
+/**
+ *
+ * @author im355
+ */
+public class Proiect {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Yo ba cane");
+    }
+}
